d33d864f7a35d08373549072a91834277cb0106e
daxantz <daxantz@gmail.com>
2024-10-21 20:12:57 -0400

    set up server

---
+++ api/app.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" });
+
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import router from "./routes/register.js";
+
+import prisma from "./prisma/database.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // Built-in middleware for JSON
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", router);
+
+async function main() {
+  app.listen(3000, (req, res) => {
+    console.log("Server running on port 3000");
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
+console.log(process.env.TEST); // Add this line in your entry file
