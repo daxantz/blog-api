d92083b4128dd2e4782762af0874dda2b9901416
daxantz <daxantz@gmail.com>
2024-10-21 20:12:13 -0400

    created handler function that creates a new user

---
+++ api/controllers/register.js
@@ -0,0 +1,23 @@
+import prisma from "../prisma/database.js";
+import bcrypt from "bcryptjs";
+import { validationResult } from "express-validator";
+async function signUp(req, res) {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log("validation error");
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { userName, email, password } = req.body;
+    const hashedPass = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: { userName: userName, email: email, password: hashedPass },
+    });
+    console.log("new user created: ", user);
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export { signUp };
